c6f2e10eaa5a82b1d5bd0929148d87ddb0fb9dbe
Abraham <wole1702@gmail.com>
2026-03-21 22:14:45 +0100

    added

--- app/model.py
+++ app/model.py
@@ -1,11 +1,14 @@
 from sqlmodel import SQLModel, Field
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from uuid import uuid4
+
+def get_adjusted_time():
+    return datetime.now(timezone.utc).replace(tzinfo=None) + timedelta(hours=1)
 
 class Post(SQLModel, table=True):
     id: str | None = Field(default_factory=lambda: str(uuid4()), primary_key=True)
     content: str
-    created_at: datetime | None = Field(default_factory=datetime.now)
+    created_at: datetime | None = Field(default_factory=get_adjusted_time)
     likes: int = 0
     views: int = 0
     comment_count: int = 0
@@ -14,5 +17,5 @@ class Comment(SQLModel, table=True):
     id: str | None = Field(default_factory=lambda: str(uuid4()), primary_key=True)
     post_id: str = Field(index=True)
     content: str
-    created_at: datetime | None = Field(default_factory=datetime.now)
+    created_at: datetime | None = Field(default_factory=get_adjusted_time)
     likes: int = 0
